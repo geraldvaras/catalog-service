ab4dc3eac61e6d1111e789585774c1173f49888d
geraldvaras <gerald.varas@gmail.com>
2024-07-16 16:04:52 -0500

    fix: Typo error on message BookAlreadyExistsException

--- src/main/java/com/polarbookshop/catalogservice/domain/BookAlreadyExistsException.java
+++ src/main/java/com/polarbookshop/catalogservice/domain/BookAlreadyExistsException.java
@@ -2,6 +2,6 @@ package com.polarbookshop.catalogservice.domain;
 
 public class BookAlreadyExistsException extends RuntimeException {
     public BookAlreadyExistsException(String isbn) {
-        super(String.format("A Book with ISBN %s already exists.", isbn));
+        super(String.format("A book with ISBN %s already exists.", isbn));
     }
 }
